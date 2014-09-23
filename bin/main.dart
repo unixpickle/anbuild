ce7ee285b29e58a6d33f1e5be958a39e09ae8da3
Alex Nichol <unixpickle@gmail.com>
2014-09-23 06:34:10 -0400

    created basic structure

---
+++ bin/main.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'dart:isolate';
+
+void main(List<String> args) {
+  if (args.length != 1) {
+    stderr.writeln('Usage: anbuild <build.dart>');
+    stderr.flush().then((_) {
+      exit(1);
+    });
+    return;
+  }
+  
+  String buildFile = args[1];
+  Isolate.spawnUri(new Uri.file(buildFile), [], null).then((isolate) {
+    // TODO: here, start listening to source file requests
+  });
+}
---
+++ lib/anbuild.dart
@@ -0,0 +1,4 @@
+/**
+ * This library will be provided to all build scripts that are run by anbuild.
+ */
+library anbuild;
